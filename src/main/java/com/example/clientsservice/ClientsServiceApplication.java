7df825fb198c356192a67d17102a989575b5f729
TSList <s.tkachenkoukr@gmail.com>
2022-11-03 19:48:30 +0200

    Init Start

--- src/main/java/com/example/clientsservice/ClientsServiceApplication.java
+++ src/main/java/com/example/clientsservice/ClientsServiceApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClientsServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientsServiceApplication.class, args);
+		System.out.println("Start");
 	}
 }

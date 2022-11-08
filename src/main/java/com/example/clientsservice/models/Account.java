3f6cdf630b53dda332fb0fee80f71254aebbb5c8
TSList <s.tkachenkoukr@gmail.com>
2022-11-08 21:43:01 +0200

    relation Client - Account

--- src/main/java/com/example/clientsservice/models/Account.java
+++ src/main/java/com/example/clientsservice/models/Account.java
@@ -3,13 +3,13 @@ package com.example.clientsservice.models;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
-@ToString
 //
 @Entity
 @Table(name = "accounts")
@@ -19,4 +19,27 @@ public class Account {
 	private Long id;
 	@Column(columnDefinition = "int default 0", nullable = false)
 	private int amount;
+	@ManyToMany(mappedBy = "accounts",fetch = FetchType.LAZY)
+	private Set<Client> clients;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Account account = (Account) o;
+		return Objects.equals(id, account.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public String toString() {
+		return "Account{" +
+			"id=" + id +
+			", amount=" + amount +
+			'}';
+	}
 }
--- src/main/java/com/example/clientsservice/models/Client.java
+++ src/main/java/com/example/clientsservice/models/Client.java
@@ -3,6 +3,7 @@ package com.example.clientsservice.models;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 @Getter
@@ -34,6 +35,29 @@ public class Client {
 	private String email;
 	@OneToMany(mappedBy = "client",fetch = FetchType.LAZY)
 	private Set<Phone> phones;
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(
+		name = "clients_accounts",
+		joinColumns = @JoinColumn(name = "client_id",nullable = false,
+			foreignKey = @ForeignKey(name = "FK_clients")),
+		inverseJoinColumns = @JoinColumn(name = "account_id",nullable = false,
+			foreignKey = @ForeignKey(name = "FK_accounts"))
+	)
+	private Set<Account> accounts;
+
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Client client = (Client) o;
+		return Objects.equals(id, client.id) && Objects.equals(surname, client.surname) && Objects.equals(name, client.name) && Objects.equals(patronymic, client.patronymic) && gender == client.gender && Objects.equals(email, client.email);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, surname, name, patronymic, gender, email);
+	}
 
 	@Override
 	public String toString() {

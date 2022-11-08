b3d66dfd9636741d7a2e1483371a910cb329696e
TSList <s.tkachenkoukr@gmail.com>
2022-11-08 20:54:57 +0200

    relation Client - Phone

--- src/main/java/com/example/clientsservice/models/Client.java
+++ src/main/java/com/example/clientsservice/models/Client.java
@@ -3,13 +3,13 @@ package com.example.clientsservice.models;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
-@ToString
+
 //
 @Entity
 @Table(name = "clients")
@@ -28,9 +28,10 @@ public class Client {
 	private String name;
 	@Column(length = 50, nullable = false)
 	private String patronymic;
+	@Column(nullable = false, columnDefinition = "int(1) default 0")
 	private Gender gender;
 	@Column(length = 50, nullable = false, unique = true)
 	private String email;
-
-
+	@OneToMany(mappedBy = "client",fetch = FetchType.LAZY)
+	private Set<Phone> phones;
 }
--- src/main/java/com/example/clientsservice/models/Phone.java
+++ src/main/java/com/example/clientsservice/models/Phone.java
@@ -8,15 +8,17 @@ import javax.persistence.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
-@ToString
 //
 @Entity
-@Table(name = "accounts")
+@Table(name = "phones")
 public class Phone {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	@Column(length = 20, nullable = false)
 	private String phone;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "client_id",nullable = false,
+		foreignKey = @ForeignKey(name = "FK_phones_clientsservice"))
+	private Client client;
 }

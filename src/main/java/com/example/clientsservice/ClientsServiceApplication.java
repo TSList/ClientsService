cb15d0d5077b0043c996124587c575d6e28caf53
TSList <s.tkachenkoukr@gmail.com>
2022-11-10 20:53:21 +0200

    repository tests Client Phone

--- src/main/java/com/example/clientsservice/ClientsServiceApplication.java
+++ src/main/java/com/example/clientsservice/ClientsServiceApplication.java
@@ -1,7 +1,17 @@
 package com.example.clientsservice;
 
+import com.example.clientsservice.models.Client;
+
+import com.example.clientsservice.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.util.List;
+
+import static com.example.clientsservice.models.Client.Gender.*;
 
 @SpringBootApplication
 public class ClientsServiceApplication {
@@ -9,4 +19,20 @@ public class ClientsServiceApplication {
 		SpringApplication.run(ClientsServiceApplication.class, args);
 
 	}
+
+	@Autowired
+	private ClientRepository clientRepository;
+
+	//@EventListener(ApplicationReadyEvent.class)
+	public void appLicationReady(){
+		Client a = new Client(0,"a","a","a", MALE,"a@tes.com"
+			,null,null,null);
+		Client b = new Client(0,"b","b","b", MALE,"b@tes.com"
+			,null,null,null);
+
+		clientRepository.saveAll(List.of(a,b));
+	}
+
+
+
 }
--- src/test/java/com/example/clientsservice/ClientsServiceApplicationTests.java
+++ src/test/java/com/example/clientsservice/ClientsServiceApplicationTests.java
@@ -1,11 +1,21 @@
 package com.example.clientsservice;
 
+import com.example.clientsservice.repositories.ClientRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ClientsServiceApplicationTests {
+
+	@Autowired
+	private ClientRepository clientRepository;
+
 	@Test
 	void contextLoads() {
+		//Assertions.assertNull(clientRepository,);
+		//Assertions.assertNull(clientRepository,"Not null!!!");
+		Assertions.assertNotNull(clientRepository,() -> String.format("%s",1111));
 	}
 }

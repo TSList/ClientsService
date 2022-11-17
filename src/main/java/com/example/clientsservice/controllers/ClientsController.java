a38a4f2109bd96c42c0200283dbd2b9a28e4c042
TSList <s.tkachenkoukr@gmail.com>
2022-11-17 23:18:32 +0200

    mustache ClientController add

--- src/main/java/com/example/clientsservice/controllers/ClientsController.java
+++ src/main/java/com/example/clientsservice/controllers/ClientsController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -25,5 +27,19 @@ public class ClientsController {
 		List<Client> clients = clientService.findAll();
 		model.addAttribute("clients",clients);
 		return "clients";
+	}
+
+	@PostMapping("clientAddForm")
+	public String clientAddForm(
+		@RequestParam("surname") String surname,
+		@RequestParam("name") String name,
+		@RequestParam("patronymic") String patronymic,
+		@RequestParam("gender") Client.Gender gender,
+		@RequestParam("email") String email) {
+
+		Client client = new Client(0,surname,name,patronymic,gender,email,null,null,null);
+		clientService.save(client);
+		return "redirect:";
+
 	}
 }

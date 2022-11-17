3b1d6b54b59669e556e7670d3bdf22f842f19002
TSList <s.tkachenkoukr@gmail.com>
2022-11-17 22:35:57 +0200

    mustache ClientController

---
+++ src/main/java/com/example/clientsservice/controllers/ClientsController.java
@@ -0,0 +1,29 @@
+package com.example.clientsservice.controllers;
+
+import com.example.clientsservice.models.Client;
+import com.example.clientsservice.services.data.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ClientsController {
+
+	@Autowired
+	private ClientService clientService;
+
+	@GetMapping("/")
+	public String load() {
+		return "redirect:clients";
+	}
+
+	@GetMapping("/clients")
+	public String loadClients(Model model) {
+		List<Client> clients = clientService.findAll();
+		model.addAttribute("clients",clients);
+		return "clients";
+	}
+}

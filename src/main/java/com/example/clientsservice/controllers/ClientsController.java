6a3bb528a2245b722dc521095da88d14640e011e
TSList <s.tkachenkoukr@gmail.com>
2022-11-22 19:27:17 +0200

    mustache ClientsController thisClientForm

--- src/main/java/com/example/clientsservice/controllers/ClientsController.java
+++ src/main/java/com/example/clientsservice/controllers/ClientsController.java
@@ -5,11 +5,14 @@ import com.example.clientsservice.services.data.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class ClientsController {
@@ -40,6 +43,12 @@ public class ClientsController {
 		Client client = new Client(0,surname,name,patronymic,gender,email,null,null,null);
 		clientService.save(client);
 		return "redirect:";
+	}
+
+	@PostMapping("thisClientForm")
+	public ModelAndView thisClientForm(@RequestParam("id") Integer id) {
+		//return new ModelAndView("clienUpdate", Map.of("clientId",id));
+		return new ModelAndView("clienUpdate", new ModelMap("clientId",id));
 
 	}
 }

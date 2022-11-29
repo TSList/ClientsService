7db5826db538e772275a62667d4d96e8c9065fd5
TSList <s.tkachenkoukr@gmail.com>
2022-11-29 20:34:37 +0200

    thymeleaf MainController ClientsController clients html openClientForm

--- src/main/java/com/example/clientsservice/controllers/ClientsController.java
+++ src/main/java/com/example/clientsservice/controllers/ClientsController.java
@@ -5,9 +5,12 @@ import com.example.clientsservice.services.data.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -29,5 +32,10 @@ private ClientService clientService;
 	public String addClientForm(@ModelAttribute Client client) {
  	clientService.save(client);
 	return "redirect:clients";
+	}
+
+	@PostMapping("openClientForm")
+	public ModelAndView openClientForm(@RequestParam("id") Integer id) {
+		return new ModelAndView("redirect:clientUpdate", new ModelMap("id",id));
 	}
 }

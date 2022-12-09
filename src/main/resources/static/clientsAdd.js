be567739e12ccca74d02d8ab41cec9dd3fe1270b
TSList <s.tkachenkoukr@gmail.com>
2022-12-09 08:55:15 +0200

    thymeleaf clients async ajax clientsObject

--- src/main/resources/static/clientsAdd.js
+++ src/main/resources/static/clientsAdd.js
@@ -22,6 +22,42 @@ const ajaxSubmitForm = () => {
     if (request.status === 200){
       let clients = JSON.parse(request.responseText);
       clients = {'clients':clients}
+      console.log(clients)
+      const html = `
+      <table>
+        <tr>
+            <th>ID</th>
+            <th>Surname</th>
+            <th>Name</th>
+            <th>Patronymic</th>
+            <th>Gender</th>
+            <th>Email</th>
+         </tr>
+        {{#clients}}
+        <tr>
+            <td>{{id}}</td>
+            <td>{{surname}}</td>
+            <td>{{name}}</td>
+            <td>{{patronymic}}</td>
+            <td>{{gender}}</td>
+            <td>{{email}}</td>
+             <td>
+              <form th:action="@{/openClientForm}" method="post">
+                <input type="hidden" name="id" value="{{id}}">
+                <input type="submit" value="&#8942;">
+              </form>
+            </td>
+        </tr>
+        {{/clients}}
+    </table>
+    
+      `
+      let result = mustache.render(html,clients);
+      let output = document.getElementById('tableOutput')
+      output.innerHTML = result;
+    }
+    else{
+      alert(request.status)
     }
 
   })
